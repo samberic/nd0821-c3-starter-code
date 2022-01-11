88068f7b4058749679f4e02aa306889840100f62
samberic <sam@samatkinson.com>
2022-01-11 09:16:42 +0000

    Fixing example

--- main.py
+++ main.py
@@ -40,16 +40,16 @@ class Value(BaseModel):
                 "workclass": "State-gov",
                 "fnlgt": "77516",
                 "education": "Bachelors",
-                "education_num": "13",
-                "marital_status": "Never-married",
+                "education-num": "13",
+                "marital-status": "Never-married",
                 "occupation": "Adm-clerical",
                 "relationship": "Not-In-family",
                 "race": "White",
                 "sex": "Male",
-                "capital_gain": 2174,
-                "capital_loss": 0,
-                "hours_per_week": 40,
-                "native_country": "United-States",
+                "capital-gain": 2174,
+                "capital-loss": 0,
+                "hours-per-week": 40,
+                "native-country": "United-States",
             }
         }
 
